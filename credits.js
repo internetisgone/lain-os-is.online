b98a14341d9097bf7585a74f15de8f9990f92840
internetisgone <schumann777@proton.me>
2022-10-16 13:20:16 +0800

    fade in credits page after bg pic is loaded, clean up css

---
+++ credits.js
@@ -0,0 +1,19 @@
+//fade in bg img when it's fully loaded
+const bgImgPath = "/img/credits-bg4-small.png";
+let mainContainer = document.getElementById("main-container");
+
+function load(src) 
+{
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.addEventListener('load', resolve);
+        image.addEventListener('error', reject);
+        image.src = src;
+    });
+}
+
+load(bgImgPath).then(() => 
+{
+    mainContainer.style.backgroundImage = `url(${bgImgPath})`;
+    mainContainer.style.opacity = "1"
+});
